2603f92405f37cec80498d79527b4a866d5e63f8
John deGuise <jrd120893@gmail.com>
2018-05-03 18:02:23 -0600

    Work on MusicTracker; renames musician to artist and extends functionality to include mutator

--- server.js
+++ server.js
@@ -4,21 +4,25 @@ var {buildSchema} = require('graphql');
 
 var schema = buildSchema(`
 	type Query {
-		musician(id: Int!): Musician 
-		musicians(name: String): [Musician]
+		artist(id: Int!): Artist
+		artists(description: String!): [Artist]
 	},
-	type Musician {
+	type Mutation {
+		updateArtistDescription(id: Int!, description: String!): Artist
+	},
+	type Artist {
 		id: Int,
 		name: String,
 		genres: [String],
 		description: String,
 		url: String,
 		instruments: [String],
-		associatedActs: [String]
+		associatedActs: [String],
+		activeYears: String
 	}
 `);
 
-var musiciansData = [
+var artistsData = [
 	{
 		id: 1,
 		name: "Tomas Kalnoky",
@@ -26,7 +30,8 @@ var musiciansData = [
 		description: "Lead singer and guitarest for Streetlight Manifesto; Solo artist; Lead singer for Bands of the Acoustic Revolution",
 		url: "https://streetlightmanifesto.com",
 		instrument: ["guitar", "ukulele", "drums", "vocals"],
-		associatedActs: ["Streetlight Manifesto", "Bandits of the Acoustic Revolution", "Tomas Kalnoky"]
+		associatedActs: ["Streetlight Manifesto", "Bandits of the Acoustic Revolution", "Tomas Kalnoky"],
+		activeYears: "1995 - Present"
 	},
 	{
 		id: 2,
@@ -35,7 +40,8 @@ var musiciansData = [
 		description: "Lead singer and guitarest for The Jimi Hendrix Experience",
 		url: "https://jimihendrix.com/jimi/",
 		instrument: ["guitar", "vocals"],
-		associatedActs: ["The Jimi Hendrix Experience", "Jimi Hendrix", "Band of Gypsies"]
+		associatedActs: ["The Jimi Hendrix Experience", "Jimi Hendrix", "Band of Gypsies"],
+		activeYears: "1963 - 1970"
 	},
 	{
 		id: 3,
@@ -44,32 +50,42 @@ var musiciansData = [
 		description: "Lead singer for Big Brother and the Holding Company; Solo artist",
 		url: "https://janisjoplin.com/",
 		instrument: ["vocals"],
-		associatedActs: ["Big Brother and the Holding Company", "Janis Joplin", "Kosmic Blues"]
+		associatedActs: ["Big Brother and the Holding Company", "Janis Joplin", "Kosmic Blues"],
+		activeYears: "1962 - 1970"
 	}
 ];
 
+var updateArtistDescription = function({id, description}) {
+	artistsData.map(artist => {
+		if(artist.id === id) {
+			artist.description = description;
+			return artist;
+		}
+	});
+	return artistsData.filter(artist => artist.id === id)[0];
+};
 
-
-var getMusician = function(args) {
+var getArtist = function(args) {
 	var id = args.id;
-	return musiciansData.filter(musician => {
-		return musician.id == id;
+	return artistsData.filter(artist => {
+		return artist.id == id;
 	})[0];
 };
 
-var getMusicians = function(args) {
+var getArtists = function(args) {
 	if (args.name) {
 		var name = args.name;
-		return musiciansData.filter(musician => musician.name == name);
+		return artistsData.filter(artist => artist.name == name);
 	} else {
-		return musiciansData;
+		return artistsData;
 	}
 }
 
 // Root resolver
 var root = {
-	musician: getMusician,
-	musicians: getMusicians
+	artist: getArtist,
+	artists: getArtists,
+	updateArtistDescription: updateArtistDescription
 	//message: () => 'Hello World!'
 };
 
