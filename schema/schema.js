32ea8fb020bd0b6def74dfd7137ab88a9f085326
John deGuise <jrd120893@gmail.com>
2018-05-06 10:13:10 -0600

    Extends basic pug template; adds support for graphiql; fixes support of inner types

--- schema/schema.js
+++ schema/schema.js
@@ -1,72 +1,127 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const graphql = require('graphql');
-const {GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema } = graphql;
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLID,
+	GraphQLSchema,
+	GraphQLList,
+	GraphQLInt
+} = graphql;
 const _ = require('lodash');
+
 // dummy data
-var artists = [
+const artists = [
 	{
-		id: 1,
+		id: '1',
 		name: "Tomas Kalnoky",
 		genres: ["ska punk", "third wave ska", "punk rock", "world", "folk"],
 		description: "Lead singer and guitarest for Streetlight Manifesto; Solo artist; Lead singer for Bands of the Acoustic Revolution",
 		url: "https://streetlightmanifesto.com",
-		instrument: ["guitar", "ukulele", "drums", "vocals"],
+		instruments: ["guitar", "ukulele", "drums", "vocals"],
 		associatedActs: ["Streetlight Manifesto", "Bandits of the Acoustic Revolution", "Tomas Kalnoky"],
 		activeYears: "1995 - Present"
 	},
 	{
-		id: 2,
+		id: '2',
 		name: "Jimi Hendrix",
 		genres: ["psychedelic rock", "rock", "hard rock", "blues", "R&B"],
 		description: "Lead singer and guitarest for The Jimi Hendrix Experience",
 		url: "https://jimihendrix.com/jimi/",
-		instrument: ["guitar", "vocals"],
+		instruments: ["guitar", "vocals"],
 		associatedActs: ["The Jimi Hendrix Experience", "Jimi Hendrix", "Band of Gypsies"],
 		activeYears: "1963 - 1970"
 	},
 	{
-		id: 3,
+		id: '3',
 		name: "Janis Joplin",
 		genres: ["psychedelic blues", "blues", "soul"],
 		description: "Lead singer for Big Brother and the Holding Company; Solo artist",
 		url: "https://janisjoplin.com/",
-		instrument: ["vocals"],
+		instruments: ["vocals"],
 		associatedActs: ["Big Brother and the Holding Company", "Janis Joplin", "Kosmic Blues"],
 		activeYears: "1962 - 1970"
 	}
 ];
 
-const ArtistType = new GraphQLObjectType({
-	name: 'Artist',
-  by: 'two',
-	fields: () => ({
-		id: { type: GraphQLID },
-		name: { type: GraphQLString },
-		genres: { type: [GraphQLString] },
-		description: { type: GraphQLString },
-		url: { type: GraphQLString },
-		instruments: { type: [GraphQLString] },
-		associatedActs: { type: [GraphQLString] },
-		activeYears: { type: GraphQLString }
-	})
+const albums = [
+	{
+		id: '1',
+		name: "Somewhere in the Between",
+		releaseYear: 2007,
+		artistId: '1'
+	},
+	{
+		id: '2',
+		name: "Axis: Bold as Love",
+		releaseYear: 1967,
+		artistId: '2'
+	},
+	{
+		id: '3',
+		name: "Big Brother and the Holding Company",
+		releaseYear: 1967,
+		artistId: '3'
+	},
+	{
+		id: '4',
+		name: "The Hands that Thieve",
+		releaseYear: 2013,
+		artistId: '1'
+	}
+]
+
+const AlbumType = new GraphQLObjectType({
+    name: 'Album',
+    fields: ( ) => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+				releaseYear: { type: GraphQLInt },
+				artist: {
+					type: ArtistType,
+					resolve(parent, args) {
+						return _.find(artists, { id: parent.artistId });
+					}
+				},
+    })
 });
-console.log(ArtistType);
+
+const ArtistType = new GraphQLObjectType({
+    name: 'Artist',
+    fields: ( ) => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        genres: { type: new GraphQLList(GraphQLString) },
+				description: { type: GraphQLString },
+				url: { type: GraphQLString },
+				instruments: { type: new GraphQLList(GraphQLString) },
+				associatedActs: { type: new GraphQLList(GraphQLString) },
+				activeYears: { type: GraphQLString },
+    })
+});
 
 const RootQuery = new GraphQLObjectType({
-	name: 'RootQueryType',
-	fields: {
-		artist: {
-			// type: ArtistType,
-			args: {id: {type: GraphQLID }},
-			resolve(parent, args) {
-				//retrieve data from db or other source
-        return _find(artists, {id: args.id})
-			}
-		}
-	}
+    name: 'RootQueryType',
+    fields: {
+        artist: {
+            type: ArtistType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args){
+                // code to get data from db / other source
+                return _.find(artists, { id: args.id });
+            }
+        },
+				album: {
+						type: AlbumType,
+						args: { id: { type: GraphQLID } },
+						resolve(parent, args) {
+							return _.find(albums, { id: args.id });
+						}
+				}
+    }
 });
 
 module.exports = new GraphQLSchema({
-	query: RootQuery
+    query: RootQuery
 });
--- server.js
+++ server.js
@@ -2,77 +2,17 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const graphql = require('graphql');
 const schema = require('./schema/schema');
-	// fields:
-	// type Query {
-	// 	artist(id: Int!): Artist
-	// 	artists(description: String!): [Artist]
-	// },
-	// type Mutation {
-	// 	updateArtistDescription(id: Int!, description: String!): Artist
-	// },
-	// type Artist {
-	// 	id: Int,
-	// 	name: String,
-	// 	genres: [String],
-	// 	description: String,
-	// 	url: String,
-	// 	instruments: [String],
-	// 	associatedActs: [String],
-	// 	activeYears: String
-	// }
-
-
-
-
-//
-// var updateArtistDescription = function({id, description}) {
-// 	artistsData.map(artist => {
-// 		if(artist.id === id) {
-// 			artist.description = description;
-// 			return artist;
-// 		}
-// 	});
-// 	return artistsData.filter(artist => artist.id === id)[0];
-// };
-//
-// var getArtist = function(args) {
-// 	var id = args.id;
-// 	return artistsData.filter(artist => {
-// 		return artist.id == id;
-// 	})[0];
-// };
-//
-// var getArtists = function(args) {
-// 	if (args.name) {
-// 		var name = args.name;
-// 		return artistsData.filter(artist => artist.name == name);
-// 	} else {
-// 		return artistsData;
-// 	}
-// }
-//
-// // Root resolver
-// var root = {
-// 	artist: getArtist,
-// 	artists: getArtists,
-// 	updateArtistDescription: updateArtistDescription
-// 	//message: () => 'Hello World!'
-// };
-
 const app = express();
+
 app.set('view engine', 'pug');
-
 app.use('/graphql', graphqlHTTP({
-	schema //required
-	// rootValue: root,
-	// graphiql: true
+	schema: schema, //required
+	graphiql: true
 }));
-// 
-// app.get('/', function(req, res) {
-// 	res.render('index', { title: 'ArtistQL', message: 'ArtistQL Landing Page: Navigate to localhost:8000/graphql to play with GraphiQL', link1: "http://www.google.com"})
-// });
-
+app.get('/', function(req, res) {
+	res.render('index', { title: 'ArtistQL', message: 'ArtistQL Landing Page: Navigate to localhost:8000/graphql to play with GraphiQL', link1: "http://localhost:8000/graphql"})
+});
 app.listen(
 	8000,
-	() => console.log("Express GraphQL Server is Now Running on localhost:8000/graphql")
+	() => {console.log("Express GraphQL Server is Now Running on localhost:8000/graphql")}
 );
