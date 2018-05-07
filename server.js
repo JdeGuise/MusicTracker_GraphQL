17fced5b0fb5a4b2a2c9b4a28bf70a36da518449
John deGuise <jrd120893@gmail.com>
2018-05-06 19:11:21 -0600

    Mongo DB support - server.js

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const graphql = require('graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
 const app = express();
+
+// connect to mongoose
+mongoose.connect("mongodb://john:test123@ds111478.mlab.com:11478/gql-ninja");
+mongoose.connection.once('open', () => {
+	console.log("Connected to database");
+});
+
+//todo: 'connect to sql' implementation
 
 app.set('view engine', 'pug');
 app.use('/graphql', graphqlHTTP({
