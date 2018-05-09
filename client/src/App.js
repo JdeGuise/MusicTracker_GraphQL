8d85d0e7777e46c790306fc5fcd21ec90deebe6c
John deGuise <john.r.deguise@gmail.com>
2018-05-09 14:51:26 -0600

    modifies label

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+
 // components
 import AlbumList from './components/AlbumList';
 import ArtistList from './components/ArtistList';
 import AddAlbum from './components/AddAlbum';
 import AddArtist from './components/AddArtist';
+
 // apollo client setup
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
@@ -16,7 +18,7 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <div id="main">
-          <h1>Goose's List of Music</h1>
+          <h1>Music Tracker</h1>
           <h2>Artist List</h2>
           <ArtistList/>
           <h2>Album List</h2>
