ab5f2426821dfd70a9bb0d8ee380f1fb4247df28
John deGuise <jrd120893@gmail.com>
2018-05-08 20:54:29 -0600

    adds support for onclick output of artists and albums

--- client/src/components/AlbumDetails.js
+++ client/src/components/AlbumDetails.js
@@ -3,11 +3,32 @@ import { graphql } from 'react-apollo';
 import { getAlbumQuery } from '../queries/queries';
 
 class AlbumDetails extends Component {
+  displayAlbumDetails() {
+    const { album } = this.props.data;
+    if(album) {
+      return(
+        <div>
+          <h1> { album.name } </h1>
+          <h2> { album.artist.name } </h2>
+          <h3> Year released: { album.releaseYear } </h3>
+          <h4>All albums by this artist:</h4>
+          <h4><ul className="other-albums">
+            { album.artist.albums.map(item => {
+              return <li key={ item.id }>{item.name}</li>
+            })}
+          </ul></h4>
+        </div>
+      )
+    } else {
+      return (
+        <div>No album selected...</div>
+      )
+    }
+  }
   render() {
-    console.log(this.props);
     return (
       <div id="album-details">
-        <p>Album details here.</p>
+        { this.displayAlbumDetails() }
       </div>
     );
   }
--- client/src/components/ArtistDetails.js
+++ client/src/components/ArtistDetails.js
@@ -3,11 +3,58 @@ import { graphql } from 'react-apollo';
 import { getArtistQuery } from '../queries/queries';
 
 class ArtistDetails extends Component {
+  displayArtistDetails() {
+    const { artist } = this.props.data;
+    if(artist) {
+      return(
+        <div>
+          <h1> { artist.name } </h1>
+          <h3>Artist Site</h3> <p> { artist.url } </p>
+          <h3> Active Years </h3> <p> { artist.activeYears } </p>
+
+          <h3> Genres </h3>
+          <ul className="artist-genres" >
+            { artist.genres.map(genre => {
+              return <li key={genre}>{genre}</li>
+            })}
+          </ul>
+
+          <h3> Instruments </h3>
+          <ul className="artist-instruments" >
+            { artist.instruments.map(instrument => {
+              return <li key={instrument}>{instrument}</li>
+            })}
+          </ul>
+
+          <h3>Associated Acts</h3>
+          <ul className="artist-associatedActs" >
+            { artist.associatedActs.map(act => {
+              return <li key={act}>{act}</li>
+            })}
+          </ul>
+
+          <h3>Description</h3> <p> { artist.description } </p>
+
+          <br/>
+          <h4>All albums by this artist:</h4>
+          <ul className="artist-albums">
+            { artist.albums.map(item => {
+              return <li key={ item.id }>{item.name}</li>
+            })}
+          </ul>
+        </div>
+      )
+    } else {
+      return (
+        <div>No artist selected...</div>
+      )
+    }
+  }
   render() {
     console.log(this.props);
     return (
       <div id="artist-details">
-        <p>Output artist details here.</p>
+        { this.displayArtistDetails() }
       </div>
     );
   }
