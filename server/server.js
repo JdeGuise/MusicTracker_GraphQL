e9bff14abfedf6f07b2c0e2e060ceecd47150c90
John deGuise <jrd120893@gmail.com>
2018-05-20 04:03:19 +0000

    Work on responsive design; whitespace fixes, media queries

--- server/server.js
+++ server/server.js
@@ -18,8 +18,8 @@ app.use(cors());
 // allow csv file upload
 app.use(fileUpload());
 app.use('/graphql', graphqlHTTP({
-	schema: schema, //required
-	graphiql: true
+  schema: schema, //required
+  graphiql: true
 }));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
@@ -32,24 +32,24 @@ app.set('view engine', 'pug');
 // connect to mongoose
 mongoose.connect(config.MONGO_DB_ADDRESS);
 mongoose.connection.on(
-	'error',
-	console.error.bind(console, 'connection error:')
+  'error',
+  console.error.bind(console, 'connection error:')
 );
 mongoose.connection.once('open', function() {
-	console.log("Connected to database");
+  console.log("Connected to database");
 });
 
 // root path setup - csv uploader
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 app.get('/template', template.get);
 app.post('/', upload.post);
 
 // listen for new connections to the server
 app.listen(
-	SERVER_PORT,
-	function() {
-		console.log("Express GraphQL Server is Now Running on " + SERVER_ADDRESS)
-	}
+  SERVER_PORT,
+  function() {
+    console.log("Express GraphQL Server is Now Running on " + SERVER_ADDRESS)
+  }
 );
